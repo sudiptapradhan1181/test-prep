8772723ecdab7cbc7d44a86c2ded2bfca3b9b12c
Sudipta Pradhan <sudipta.pradhan1@whitehatjr.com>
2024-03-14 12:28:00 +0530

    singleton pattern updated

--- designPatterns.js
+++ designPatterns.js
@@ -70,8 +70,41 @@ const Singleton = (function(){
 
 const processManager1 = Singleton.getProcessManager()
 const processManager2 = Singleton.getProcessManager()
-
 // console.log(processManager1 === processManager2)
+
+
+// 1. Creating the `Counter` class, which contains a `constructor`, `getInstance`, `getCount`, `increment` and `decrement` method.
+// Within the constructor, we check to make sure the class hasn't already been instantiated.
+class Counter {
+    constructor() {
+      if (instance) {
+        throw new Error("You can only create one instance!");
+      }
+      this.counter = counter;
+      instance = this;
+    }
+  
+    getCount() {
+      return this.counter;
+    }
+  
+    increment() {
+      return ++this.counter;
+    }
+  
+    decrement() {
+      return --this.counter;
+    }
+  }
+  
+  // 2. Setting a variable equal to the the frozen newly instantiated object, by using the built-in `Object.freeze` method.
+  // This ensures that the newly created instance is not modifiable.
+  const singletonCounter = Object.freeze(new Counter());
+  
+//   // 3. Exporting the variable as the `default` value within the file to make it globally accessible.
+//   export default singletonCounter;
+
+
 
 //Strategy Pattern
 function Fedex () {
